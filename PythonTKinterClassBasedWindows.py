43f4db976e311bd05b3fe45f22a3ff539f7b74d9
Christopher Osborn <xinjalin@gmail.com>
2022-05-18 00:50:06 +0930

    v0.5 working window swap from successful login to fishing game window
    the main fishing app file is PythonTkinterClassWindows.py
    
    reading csv file
    
    tidy up code look added comments to describe what parts of the program are

--- PythonTKinterClassBasedWindows.py
+++ PythonTKinterClassBasedWindows.py
@@ -19,32 +19,30 @@ def load_fish_data():
         print(list_of_dict)
 
 
+# -------------------------------------------------------------------------------------------------------------------- #
 class WindowLogin:
-
+    # Class constructor
     def __init__(self, root, title):
         self.root = root
         self.root.title(title)
-
+        # username
         self.username_label = Label(self.root, text="Username")
         self.username_label.grid(row=0, column=0, padx=10, pady=5)
-
         self.username_entry = Entry(self.root, width=30)
         self.username_entry.grid(row=0, column=1, padx=10, pady=5)
-
+        # password
         self.password_label = Label(self.root, text="Password")
         self.password_label.grid(row=1, column=0, padx=10, pady=5)
-
         self.password_entry = Entry(self.root, width=30, show="*")
         self.password_entry.grid(row=1, column=1, padx=10, pady=5)
-
+        # show password toggle
         self.myCheckButton = Checkbutton(text="show password", command=self.show_password)
         self.myCheckButton.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
-
+        # login button
         self.myButton = Button(self.root, text="login", width=10, command=self.login)
         self.myButton.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
-
+        # GUI loop
         self.root.mainloop()
-        pass
 
     def show_password(self):
 
@@ -54,7 +52,8 @@ class WindowLogin:
             self.password_entry.config(show="*")
 
     def login(self):
-
+        # username = admin
+        # password = admin
         if self.username_entry.get() == "admin" and self.password_entry.get() == "admin":
             print("Login successful")
             # open new window
@@ -65,12 +64,13 @@ class WindowLogin:
             print("Login failed")
 
 
+# -------------------------------------------------------------------------------------------------------------------- #
 class WindowFishGame:
-
+    # Class constructor
     def __init__(self, root, title, geometry):
         self.root = root
         self.root.title(title)
-        self.root.geometry(geometry)  # formatted as "width x height"
+        self.root.geometry(geometry)  # 400x400
 
         self.fish_name_label = Label(self.root, text="Fish Name")
         self.fish_name_label.pack(side=LEFT, padx=10, pady=5)
